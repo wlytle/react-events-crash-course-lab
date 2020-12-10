c8d37235e821fc7cc3f1de995a5a03b2c58d615b
wlytle <wlytle89@gmail.com>
2020-12-10 16:13:13 -0700

    commit tree 084be82b8d10d145467047fddd9b56fa94dfe8da

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -1,37 +1,54 @@
-import React, { Component } from 'react';
-import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js'
-
+import React, { Component } from "react";
+import {
+  drawChromeBoiAtCoords,
+  toggleCycling,
+  resize,
+} from "./canvasHelpers.js";
 
 export default class ChromeBoisDomain extends Component {
-  
   handleMouseMove = (event) => {
     /* TODO: This method should capture the `x` and `y` coordinates of the mouse
      * from the event and use them to invoke the `drawChromeBoiAtCoords`
      * function that has been provided and is already imported
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
-  }
-  
+    drawChromeBoiAtCoords(event.clientX, event.clientY);
+  };
+
   /* TODO: Create an event handler which, when fired, invokes the provided
    * `toggleCycling` function with no arguments. Don't forget the click event
    * listener that should fire it!
    */
-   
+  handleClick = () => {
+    toggleCycling();
+  };
+
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
   /* is pressed. When a key is pressed, an event handler should invoke the
   /* provided `resize` function with a single argument of either '+' or '-'
   /* if the key pressed was 'a', then it should call `resize` with '+'
   /* if the key pressed was 's', then it should call `resize` with '-' 
    */
-  
+  handleKeyPress = (e) => {
+    if (e.key !== "a" && e.key !== "s") return;
+    let keyGuide = {
+      a: "+",
+      s: "-",
+    };
+
+    resize(keyGuide[e.key]);
+  };
+
   render() {
     return (
-      <canvas 
+      <canvas
         onMouseMove={this.handleMouseMove}
-        width='900'
-        height='600'
-        tabIndex="0">
-      </canvas>
-    )
+        width="900"
+        height="600"
+        tabIndex="0"
+        onClick={this.handleClick}
+        onKeyPress={this.handleKeyPress}
+      ></canvas>
+    );
   }
 }
